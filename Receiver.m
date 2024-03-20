4b4b368d8a166719d335d9b1fc250e5376f4ef4a
drewberz <andrew_berzolla@brown.edu>
2024-03-19 20:07:30 -0400

    Repushing "receiver" but no longer as a function

--- Receiver.m
+++ Receiver.m
@@ -1,6 +1,6 @@
-function [secret_message, last_message] = Receiver(extract_msg, key_decode)
+%function [secret_message, last_message] = Receiver(coded_img, img)
 
 secret_message = extract_msg(coded_img, img);
 last_message = key_decode(secret_message)
 
-end
+%end

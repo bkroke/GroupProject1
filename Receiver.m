52785c0f08da80eb7328d9f4225c7c5fc766ff33
bkroke <brynn_kroke@brown.edu>
2024-03-19 20:31:06 -0400

    changed variable names and added comments

--- Receiver.m
+++ Receiver.m
@@ -1,6 +1,6 @@
 %function [secret_message, last_message] = Receiver(coded_img, img)
 
-secret_message = extract_msg(coded_img, img);
-last_message = key_decode(secret_message)
+extracted_message = extract_msg(coded_img, img);
+last_message = key_decode(extracted_message)
 
 %end
--- extract_msg.m
+++ extract_msg.m
@@ -1,14 +1,14 @@
-function secret_message = extract_msg(coded_img, img)
-secret_message = [];
+function extracted_message = extract_msg(coded_img, img)
+extracted_message = [];
     for row = 1:size(coded_img, 1)
         for col = 1:size(coded_img, 2)
             if ~isequal(coded_img(row, col, 1), img(row, col, 1))
-                secret_message = [secret_message coded_img(row, col, 1)];
-                
+                extracted_message = [extracted_message coded_img(row, col, 1)]; 
+                %if the red channel of coded_img does not equal the red
+                %channel for img for a given element, that value is added
+                %to extracted_msg
             end
             
         end
     end 
-%num_char2 = secret_message(1); %this will be used to tell the unscrambler how many times to run itself
-%secret_message(1) = [];
 end

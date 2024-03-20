c11799fb9f1e94df2d3fc30910cee76555b09cfc
drewberz <andrew_berzolla@brown.edu>
2024-03-19 20:44:34 -0400

    Cleaned up variable names in sender and scramble_msg

--- sender.m
+++ sender.m
@@ -6,11 +6,11 @@
 %image stored in it for now
 
 img = imread("Cat_Dive.png");
-sec_msg = 0;
+initial_msg = 0;
 
 
 img = imread("Cat_Dive.png");
 coded_img = img;
 
-[flipnew_sec_msg] = scramble_msg(sec_msg);
-[coded_img, img] = input_msg(flipnew_sec_msg);
+[coded_msg] = scramble_msg(initial_msg);
+[coded_img, img] = input_msg(coded_msg);
